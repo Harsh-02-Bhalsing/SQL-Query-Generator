7cdfdf4eae1b9dd0e384cd9b3b837907d5045ac0
Harsh-02-Bhalsing <bhalsingharshad@gmail.com>
2026-01-25 22:50:38 +0530

    added dashboard page which contains placeholder compnents

---
+++ Frontend/src/components/QueryOutputPanel.jsx
@@ -0,0 +1,9 @@
+const QueryOutputPanel = () => {
+  return (
+    <div className="h-full rounded-md bg-black border border-[#333] p-4 text-green-400 font-mono text-sm">
+      Query Output / Terminal Area updated
+    </div>
+  );
+};
+
+export default QueryOutputPanel;
